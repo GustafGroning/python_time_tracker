9eef9039ece714972efe341f97fb2bbe061b7966
Gustaf Gröning <gustaf.groning.8601@student.uu.se>
2021-03-02 19:34:38 +0100

    new fixes

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
-
+import json
 from AppKit import NSWorkspace
 import time
-#active_app_name = NSWorkspace.sharedWorkspace().frontmostApplication().localizedName()
-#print (active_app_name)
 
-
-
-#from distutils.sysconfig import get_python_lib
-#print(get_python_lib())
-
+testList = ["Trello", "Safari"]
 def check_window(): 
-    active_window = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
+    first_window = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
     while True:
         new_window = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
-        if new_window != active_window:
+        if new_window != first_window:
             check_category(new_window)
         else:
             print("no change")
-            time.sleep(2)
+            time.sleep(1)
 # om användaren byter fönster, skicka till check_category
 
 
 def check_category(new_window):
-    if new_window == "Trello":
-        print ("you're in trello, good job!")
-        time.sleep(2)
-        check_window()
+    if new_window in testList:
+        print("this works!")
+        time.sleep(1)
     else:
         print ("why are you not working stupid?")
-        time.sleep(2)
+        time.sleep(1)
         check_window()
+
+
 
 # kollar om det nya fönstret/hemsidan hör till en kategori
 # om NEJ, skicka tillbaka till check_window (för det här programmet ska inte trackas, check_window får då gå för evigt)
 # om JA, skicka till timer
 
 
-def timer():
-    pass
+def timer(new_window):
+    start_time = time.time()
+    while True:
+        newer_window = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
+        elapsed_time = time.time() - start_time
+        if new_window != newer_window:
+            print("you spent", elapsed_time, "in trello, good job!")
+            check_window()
+        else:
+            time.sleep(1)
+    
+    
 #timear sekunder som användaren är i fönstret
 #om användaren byter fönster, skicka till time_log
 
@@ -47,4 +52,26 @@ def time_log():
 # spara tid spenderad och kategori i JSON
 # skicka till check_window
 
-check_window()
+def add_category(category, appOrSite): # user inputs the category the new app or site is going into, followed by the full name of the app/site.
+    jsonInput = {"category": category, "appOrSite": appOrSite}
+    with open("categories.json", "w") as f:
+        json.dump(jsonInput, f)
+#TODO: just nu skriver add_category över tidigare rader, det ska den inte göra.
+
+#check_window()
+
+f = open('categories.json',) 
+  
+# returns JSON object as  
+# a dictionary 
+data = json.load(f) 
+  
+# Iterating through the json list
+#for i in data['categories']: 
+ #   print(i) 
+  
+print(data['categories'])
+
+# Closing file 
+f.close() 
+
