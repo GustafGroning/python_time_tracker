a17807693796b58121b29216b7fa89bd42d9ec9d
Gustaf Gröning <gustafgroning@gmail.com>
2021-03-08 18:32:56 +0100

    seperated timer functions to separate file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 from AppKit import NSWorkspace
 import time
+import timer as t
+
 
 testList = ["Trello", "Safari"]
 def check_window(): 
@@ -14,10 +16,20 @@ def check_window():
             time.sleep(1)
 # om användaren byter fönster, skicka till check_category
 
-
 def check_category(new_window):
-    if new_window in testList:
-        print("this works!")
+    f = open('categories.json',) 
+    # returns JSON object as a dictionary 
+
+    data = json.load(f) 
+    categoryList = [] #stores all categories so the timer knows how to store the data.
+
+    for item in data["categories"]:
+        categoryList.append(item["appOrSite"])
+    print(categoryList)
+
+
+    if new_window in categoryList: #Safari, Onenote, Trello
+        t.timer(new_window, category) #TODO: skapa category, det ska vara kategorin som 
         time.sleep(1)
     else:
         print ("why are you not working stupid?")
@@ -25,40 +37,14 @@ def check_category(new_window):
         check_window()
 
 
-
-# kollar om det nya fönstret/hemsidan hör till en kategori
-# om NEJ, skicka tillbaka till check_window (för det här programmet ska inte trackas, check_window får då gå för evigt)
-# om JA, skicka till timer
-
-
-def timer(new_window):
-    start_time = time.time()
-    while True:
-        newer_window = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
-        elapsed_time = time.time() - start_time
-        if new_window != newer_window:
-            print("you spent", elapsed_time, "in trello, good job!")
-            check_window()
-        else:
-            time.sleep(1)
-    
-    
-#timear sekunder som användaren är i fönstret
-#om användaren byter fönster, skicka till time_log
-
-
-def time_log():
-    pass
-# spara tid spenderad och kategori i JSON
-# skicka till check_window
-
 def add_category(category, appOrSite): # user inputs the category the new app or site is going into, followed by the full name of the app/site.
     jsonInput = {"category": category, "appOrSite": appOrSite}
     with open("categories.json", "w") as f:
         json.dump(jsonInput, f)
 #TODO: just nu skriver add_category över tidigare rader, det ska den inte göra.
 
-#check_window()
+
+"""
 # pröva en grej
 f = open('categories.json',) 
 
@@ -71,7 +57,9 @@ data = json.load(f)
  #   print(i) 
 
 for i in range(0, 3):
-    print(data['categories'])
+    print(data['categories'][i]['appOrSite'])
 
 f.close() 
+"""
 
+check_window()
