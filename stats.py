6a16dbb923c8d25982dade4f0627ef801484ba18
Gustaf Gröning <gustafgroning@gmail.com>
2021-03-10 10:35:53 +0100

    finished data gathering, started work on pie chart

--- stats.py
+++ stats.py
@@ -6,24 +6,45 @@ import json
 f = open('timeLog.json',) 
 data = json.load(f) 
 
-for i in data['timeSaved']:
-    print(i['timeInMinutes']) #TODO: nu printar den alla värden i ordning! Nu ska den göra det enligt kategori och plusa, AKA ta allt "work" och lägg ihop.
+def get_stats():
+    workList = []
+    funList = []
+    wasteList = []
+
+    for i in data['timeSaved']:
+        if (i)['category'] == "work": # i blir alltså raden som den printar ifrån, timeInMinutes väljer vilket värde som ska printas.
+            workList.append(i['timeInMinutes'])
+        
+
+        if (i)['category'] == "fun":
+            funList.append(i['timeInMinutes'])
+        
+
+        if (i)['category'] == "waste":
+            wasteList.append(i['timeInMinutes'])
+        
+
+    print("you have worked for ", sum(workList))
+    print("you had fun for ", sum(funList))
+    print("you wasted ", sum(wasteList))
+    pie_chart(workList, funList, wasteList)
 
 
 
-
-"""
+def pie_chart(workList,funList,wasteList):
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    labels = 'work', 'fun', 'waste'
+    sizes = [workList, funList, wasteList]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'fun')
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-#plt.show()
-"""
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.show()
+
+
+get_stats()
 
 """
 cirkeldiagram med tid spenderat i olika kategorier
